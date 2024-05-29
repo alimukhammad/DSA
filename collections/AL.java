032384758ff0552bbf4ab9a6c887b0f8bb50fdcd
sardor <alimukhammad1@gmail.com>
2024-05-28 23:32:24 -0400

    3

---
+++ collections/AL.java
@@ -0,0 +1,41 @@
+package collections;
+
+// Java program to demonstrate the
+// working of ArrayList
+import java.io.*;
+import java.util.*;
+
+class GFG {
+
+	// Main Method
+	public static void main(String[] args)
+	{
+
+		// Declaring the ArrayList with
+		// initial size n
+		ArrayList<Integer> al = new ArrayList<Integer>();
+
+		// Appending new elements at
+		// the end of the list
+		for (int i = 1; i <= 5; i++)
+			al.add(i);
+
+		// Printing elements
+		System.out.println(al);
+
+		// Remove element at index 3
+		al.remove(3);
+
+		// Displaying the ArrayList
+		// after deletion
+		System.out.println(al);
+
+		// Printing elements one by one
+		for (int i = 0; i < al.size(); i++)
+			System.out.print(al.get(i) + " ");
+	}
+}
+
+// [1, 2, 3, 4, 5]
+// [1, 2, 3, 5]
+// 1 2 3 5 
